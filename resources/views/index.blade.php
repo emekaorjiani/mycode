8e2027a4ef6acc4f306834603ad35a2ecbe11d79
Orjiani Chukwuemeka <emekaorjiani@gmail.com>
2021-11-10 15:07:24 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,5 +14,12 @@
             </div>
         </div>
     </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Hello World 3</h1>
+        </div>
+    </div>
+</div>
 </body>
 </html>
